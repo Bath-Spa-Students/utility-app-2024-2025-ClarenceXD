91bbabe49e95f26a33c41cd684dcd75260df17c0
ClarenceXD <dsouza.clarence2007@gmail.com>
2025-01-08 13:58:29 +0400

    Utility App
    Update log
    
    -Changed item prices
    
    -Made the text more simple when the stock has ran out during your purchase

--- Utility App.py
+++ Utility App.py
@@ -6,102 +6,102 @@ A00 = {
   "A01" : {
     "name" : "Potato Chips",
     "stock" : 3,
-    "cost" : 10
+    "cost" : 5
   },
   "A02" : {
     "name" : "Crackers",
     "stock" : 6,
-    "cost" : 10
+    "cost" : 6
   },
   "A03" : {
     "name" : "peanuts",
     "stock" : 8,
-    "cost" : 15
+    "cost" : 8
   },
   "A04" : {
     "name" : "Pretzels (salted)",
     "stock" : 2,
-    "cost" : 20
+    "cost" : 10
   },
   "A05" : {
     "name" : "Trail Mix",
     "stock" : 4,
-    "cost" : 20
+    "cost" : 6
   },
   "B01" : {
     "name" : "Protein Bars",
     "stock" : 10,
-    "cost" : 5
+    "cost" : 3
   },
   "B02" : {
     "name" : "Cookies",
     "stock" : 2,
-    "cost" : 10
+    "cost" : 6
   },
   "B03" : {
     "name" : "Muffins (3)",
     "stock" : 4,
-    "cost" : 20
+    "cost" : 10
   },
   "B04" : {
     "name" : "Chocolate-Covered Almonds",
     "stock" : 2,
-    "cost" : 25
+    "cost" : 8
   },
   "B05" : {
     "name" : "Gum Ball",
     "stock" : 4,
-    "cost" : 10
+    "cost" : 2
   },
   "C01" : {
     "name" : "Mtn Dew (250ml)",
     "stock" : 4,
-    "cost" : 15
+    "cost" : 12
   },
   "C02" : {
     "name" : "Ice Tea (250ml)",
     "stock" : 2,
-    "cost" : 25
+    "cost" : 8
   },
   "C03" : {
     "name" : "Iced Coffee (200ml)",
     "stock" : 7,
-    "cost" : 25
+    "cost" : 10
   },
   "C04" : {
     "name" : "Energy Drink (250ml)",
     "stock" : 7,
-    "cost" : 20
+    "cost" : 12
   },
   "C05" : {
     "name" : "Lemonade (250ml)",
     "stock" : 7,
-    "cost" : 20
+    "cost" : 12
   },
   "D01" : {
     "name" : "Hot Chocolate (200ml)",
     "stock" : 2,
-    "cost" : 20
+    "cost" : 6
   },
   "D02" : {
     "name" : "Coffee (200ml)",
     "stock" : 3,
-    "cost" : 25
+    "cost" : 10
   },
   "D03" : {
     "name" : "Tea (200ml)",
     "stock" : 2,
-    "cost" : 25
+    "cost" : 8
   },
   "D04" : {
     "name" : "Black Tea (200ml)",
     "stock" : 3,
-    "cost" : 20
+    "cost" : 6
   },
   "D05" : {
     "name" : "Espresso (200ml)",
     "stock" : 3,
-    "cost" : 30
+    "cost" : 12
   }
 }
 #Creates the menu with current values
@@ -136,7 +136,7 @@ print("""
   ╚═══╝  ╚══════╝╚═╝  ╚═══╝╚═════╝ ╚═╝╚═╝  ╚═══╝ ╚═════╝     ╚═╝     ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝╚══════╝
                                                                                                                      """)
 display_menu()
-user_money = int(input("Insert Money (avg price per item is 25$): "))
+user_money = int(input("Insert Money (avg price per item is 12$): "))
 #using loop to allow user to buy multiple items
 while True:                                                                         
     user_input = str(input("-Exit(0)--Menu(1)--Insert_Money(2)--Enter Item Code: ").capitalize())      
@@ -153,7 +153,7 @@ while True:
     elif user_input in A00:
         #checks stock of item
         if A00[(user_input)]["stock"] == 0:
-            print(f"⚠️  The {A00[(user_input)]['name']} stock has ran out, please buy another item ⚠️\n")
+            print(f"⚠️  The {A00[(user_input)]['name']} stock has run out, please buy another item ⚠️\n")
         elif user_money >= A00[(user_input)]["cost"]:                                 
             user_money -= A00[(user_input)]["cost"]                                  
             A00[(user_input)]["stock"] -=1
